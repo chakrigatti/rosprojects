30c7ffdc346f11175f49fe731de1c32474218ca2
chakradhar.g <chakradhar.g@zohocorp.com>
2024-09-09 19:11:15 +0000

    Created an action server, action client and action interface

---
+++ ros2_ws/src/actions_py/actions_py/count_until_client.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from rclpy.action.client import ClientGoalHandle,GoalStatus
+from my_robot_interfaces.action import CountUntil
+
+
+class CountUntilClientNode(Node):
+    def __init__(self):
+        super().__init__("count_until_client")
+        self.count_until_client_ = ActionClient(self,CountUntil,"count_until")
+        
+    def send_goal(self,target_number,period):
+        self.count_until_client_.wait_for_server()
+
+        goal = CountUntil.Goal()
+        goal.target_number = target_number
+        goal.period = period
+
+        self.get_logger().info("Sending the goal")
+        self.count_until_client_. \
+            send_goal_async(goal,feedback_callback=self.goal_feedback_callback). \
+            add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self,future):
+        self.goal_handle_:ClientGoalHandle  = future.result()
+        if self.goal_handle_.accepted:
+            self.get_logger().info("Goal got accepted")
+            self.goal_handle_.get_result_async().add_done_callback(self.goal_result_callback)
+        else:
+            self.get_logger().warn("Goal got rejected")
+
+    def goal_result_callback(self,future):
+        status = future.result().status
+        result = future.result().result
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info("Success")
+        elif status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().error("Aborted")
+        elif status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().warn("Cancelled")
+        
+        self.get_logger().info("Result : "+str(result.reached_number))
+
+    def goal_feedback_callback(self,feedback_msg):
+        number = feedback_msg.feedback.current_number
+        # if number == 5 :
+        #     self.get_logger().info("Sent a cancel request")
+        #     self.goal_handle_.cancel_goal_async()
+        self.get_logger().info("Got Feedback " + str(number))
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CountUntilClientNode()
+    node.send_goal(10,1.0)
+    rclpy.spin(node)
+    rclpy.shutdown()
+    
+    
+if __name__ == "__main__":
+    main()
